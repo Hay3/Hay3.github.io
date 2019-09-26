dd1997b0699a0f307addb4e163cf880161b8e640
Hay3 <hurhyuok@gmail.com>
2019-09-25 21:07:08 -0700

    no message

--- fetch.js
+++ fetch.js
@@ -1,3 +1,19 @@
 function loadArtistInfo() {
-    var table = document.getElementByClassName("artistTable");
+    var table = document.getElementsByTagName("table");
+    table.append('111111111111');
+
+    var i;
+    for (i = 0; i < 5; ++i) {
+        let trNode = document.createElement("tr");
+        let img = document.createElement("img");
+
+        const id = Math.floor(Math.random() * 100);
+        const sexID = Math.floor(Math.random() * 2) <= 1 ? "men" : "women";
+
+        img.src = 'https://randomuser.me/api/portraits/med/${sexID}/${id}.jpg';
+        console.log(img.src);
+
+        trNode.appendChild(img);
+        table.append(img);
+    }
 }
