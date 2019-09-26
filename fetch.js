1a4b2521a6bad6700cd3727560cc60e293a18535
Hay3 <hurhyuok@gmail.com>
2019-09-25 20:54:26 -0700

    no message

--- fetch.js
+++ fetch.js
@@ -1,20 +1,3 @@
 function loadArtistInfo() {
     var table = document.getElementByClassName("artistTable");
-
-    console.log(1);
-
-    var i;
-    for (i = 0; i < 5; ++i) {
-        let trNode = document.createElement("tr");
-        let img = document.createElement("img");
-
-        const id = Math.floor(Math.random() * 100);
-        const sexID = Math.floor(Math.random() * 2) <= 1 ? "men" : "women";
-
-        img.src = 'https://randomuser.me/api/portraits/med/${sexID}/${id}.jpg';
-        console.log(img.src);
-
-        trNode.appendChild(img);
-        table.append(img);
-    }
 }
